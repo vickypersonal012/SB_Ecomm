c5a403088d2eab3396d26e8923517cee490d0075
vicky tuli <vickytuli20@gmail.com>
2025-10-01 19:55:02 +0530

    Initial Project Set up

---
+++ src/main/java/com/ecommerce/sb_ecomm/SbEcommApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sb_ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/sb_ecomm/SbEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sb_ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
