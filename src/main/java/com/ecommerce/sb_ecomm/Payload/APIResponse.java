8987a076881805c5a60fcca1c5bcde87b9c0252e
vicky tuli <vickytuli20@gmail.com>
2025-10-04 21:03:34 +0530

    Added API Response File for better error responses

---
+++ src/main/java/com/ecommerce/sb_ecomm/Payload/APIResponse.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sb_ecomm.Payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponse {
+    public String message;
+    public boolean status;
+}
